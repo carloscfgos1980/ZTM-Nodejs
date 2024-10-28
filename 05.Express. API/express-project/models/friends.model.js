4f880d468693321ae6a2d4793608358e32b0a44c
Carlos Infante <kuva5008@gmail.com>
2024-10-28 12:53:45 +0100

    chapter 5

---
+++ 05.Express. API/express-project/models/friends.model.js
@@ -0,0 +1,13 @@
+//Data
+const friends = [
+    {
+        id:0,
+        name: 'Albert Einstein'
+    },
+    {
+        id:1,
+        name: 'Sir Issac Newtoon'
+    }
+]
+
+module.exports = friends
